dbb3f234a5b89a2a9334844c9608bb339b032f93
sana <sana@NakamuranoMacBook-Pro.local>
2021-06-17 18:57:25 +0900

    implement callBack with useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React,{useState} from 'react'
+import React,{ useEffect ,useState} from 'react'
 
 const App = (props) => {
   const [state,setState] = useState(props)
   const {name,price} = state
+
+  useEffect(() => {
+    //DOMが変更されるたびにレンダリングされる
+    console.log("This is like componentDidMount or componentDidUpdate")
+  })
+  useEffect(() => {
+    //空の配列を渡すことで際レンダリングを防止できる
+    console.log("This is like componentDidMount")
+  },[])
+  useEffect(() => {
+    //stateの名称を渡すことで変更のたびにレンダリング
+    console.log("This callBack is for name only")
+  },[name])
   
 const reset = () => {
-
   setState(props)
 }
   return (
