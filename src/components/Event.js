cfd79adc24237821613beea2a77c911c7b8ed08b
nakamura224 <dq.tri.fi@gmail.com>
2021-07-12 15:23:39 +0900

    solve prop drilling issue

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import {DELETE_EVENT} from '../actions'
-const Event =({dispatch,event}) =>{
-  
+import AppContext from '../contexts/AppContext'
+
+const Event =({ event }) =>{
+  const { dispatch } = useContext(AppContext)
   const handleClickDeleteButton=()=>{
     const result = window.confirm(`イベント(id = ${event.id})を本当に削除してもよろしいですか？`)
     if (result) dispatch({type:'DELETE_EVENT',id:event.id})
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,12 +1,14 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import {
   CREATE_EVENT,
   DELETE_ALL_EVENTS
 } from '../actions'
+import AppContext from '../contexts/AppContext'
 
-const EventForm = ({state,dispatch}) =>{
-  const [title,setTitle] = useState('')
-  const [body,setBody] = useState('')
+const EventForm = () =>{
+  const { state, dispatch } = useContext(AppContext)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
 
   const addEvent = (e) => {
     e.preventDefault()//画面の再レンダリングを防止できる
