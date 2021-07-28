83ced577e13b5b205634e883686c1c004d614188
nakamura224 <dq.tri.fi@gmail.com>
2021-07-28 15:11:42 +0900

    create iso8601 format time

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useReducer} from 'react'
+import React,{useReducer, useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from './EventForm'
 import reducer from '../reducers'
@@ -7,13 +7,14 @@ import AppContext from '../contexts/AppContext'
 
 
 const App = () => {
-  const initialState = {
-
+  //const [state,setState] = useState(props)
+  const initialState = {//まず状態を初期化
     events: [],
     operationLogs:[]
   }
   const [state,dispatch] = useReducer(reducer,initialState)
-
+  //const output = useState(0) 配列(2つの要素)を返す
+  //const [count,setCount] = useState(0)//分割代入で代入されるものに名前をつける.setCountはcountを操作する関数
   return(
   <AppContext.Provider value = {{ state,dispatch}}>
     <div className ="container-fluid">
