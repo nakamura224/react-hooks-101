9be31fa09db357dad9e18857bbecb05a1f50610c
nakamura224 <dq.tri.fi@gmail.com>
2021-07-29 14:03:39 +0900

    save state with localstrage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useReducer, useState} from 'react'
+import React,{useReducer, useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from './EventForm'
 import reducer from '../reducers'
@@ -7,16 +7,23 @@ import AppContext from '../contexts/AppContext'
 import OperationLogs from './OperationLogs'
 
 
-
+const APP_KEY = 'appWithRedux'
 const App = () => {
+  const appState = localStorage.getItem(APP_KEY)
   //const [state,setState] = useState(props)
-  const initialState = {//まず状態を初期化
+  const initialState = appState ? JSON.parse(appState) : {//まず状態を初期化 string型をオブジェクトの型に戻す
     events: [],
     operationLogs:[]
   }
   const [state,dispatch] = useReducer(reducer,initialState)
   //const output = useState(0) 配列(2つの要素)を返す
   //const [count,setCount] = useState(0)//分割代入で代入されるものに名前をつける.setCountはcountを操作する関数
+  useEffect(() => {
+     //localstrageへはstrign型にして保存する
+     localStorage.setItem(APP_KEY, JSON.stringify(state))
+    },[state])
+
+
   return(
   <AppContext.Provider value = {{ state,dispatch}}>
     <div className ="container-fluid">
